ad7f821769834cc527f0e4ba2ac6cdab5eaac86d
Kristin <kristindyulgeryan@abv.bg>
2025-03-11 16:24:13 +0200

    List games in catalog page

---
+++ client/src/components/game-catalog/game-catalog-item/GameCatalogItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function GameCatalogItem( {
+    title, category, imageUrl
+}) {
+  return (
+    <div className="allGames">
+    <div className="allGames-info">
+      <img src={imageUrl} />
+      <h6>{category}</h6>
+      <h2>{title}</h2>
+      <a href="#" className="details-button">
+        Details
+      </a>
+    </div>
+  </div>
+  )
+}
+
+export default GameCatalogItem
