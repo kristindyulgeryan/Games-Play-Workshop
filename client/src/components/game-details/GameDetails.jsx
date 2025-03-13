b8882f236a6fa30b40428afb3aa555491851a770
Kristin <kristindyulgeryan@abv.bg>
2025-03-13 21:28:28 +0200

    Extract comments related jsx in separate components

--- client/src/components/game-details/GameDetails.jsx
+++ client/src/components/game-details/GameDetails.jsx
@@ -1,89 +1,58 @@
-import { useEffect, useState } from 'react'
-import { Link, useNavigate, useParams } from 'react-router'
-import gameService from '../../services/gameService.js'
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router";
+import gameService from "../../services/gameService.js";
+import CommentsShow from "../comments-show/CommentsShow.jsx";
+import CommentsCreate from "../comments-add/CommentsCreate.jsx";
 
 export default function GameDetails() {
-const navigate = useNavigate()
-  const[game, setGame] = useState({})
-  const {gameId} = useParams()
+  const navigate = useNavigate();
+  const [game, setGame] = useState({});
+  const { gameId } = useParams();
 
-
-useEffect(()=>{
-(async () => {
-  const result = await gameService.getOne(gameId)
-  setGame(result)
-})();
-  }, [gameId])
+  useEffect(() => {
+    (async () => {
+      const result = await gameService.getOne(gameId);
+      setGame(result);
+    })();
+  }, [gameId]);
   console.log(game);
 
+  const gameDeleteClickHandler = async () => {
+    const hasConfirm = confirm(
+      `Are you sure you want to delete ${game.title} game?`
+    );
 
-const gameDeleteClickHandler = async()=>{
-  const hasConfirm = confirm(`Are you sure you want to delete ${game.title} game?`)
+    if (!hasConfirm) {
+      return;
+    }
 
- if(!hasConfirm){
-  return
- }
-
- await gameService.delete(gameId)
- navigate('/')
-}
+    await gameService.delete(gameId);
+    navigate("/");
+  };
 
   return (
     <section id="game-details">
-    <h1>Game Details</h1>
-    <div className="info-section">
-      <div className="game-header">
-        <img className="game-img" src={game.imageUrl} />
-        <h1>{game.title}</h1>
-        <span className="levels">MaxLevel: {game.maxLevel}</span>
-        <p className="type">{game.category}</p>
+      <h1>Game Details</h1>
+      <div className="info-section">
+        <div className="game-header">
+          <img className="game-img" src={game.imageUrl} />
+          <h1>{game.title}</h1>
+          <span className="levels">MaxLevel: {game.maxLevel}</span>
+          <p className="type">{game.category}</p>
+        </div>
+        <p className="text">{game.summary}</p>
+        <CommentsShow />
+        {/* Edit/Delete buttons ( Only htmlFor= creator of this game )  */}
+        <div className="buttons">
+          <Link to={`/games/${gameId}/edit`} className="button">
+            Edit
+          </Link>
+          <button onClick={gameDeleteClickHandler} className="button">
+            Delete
+          </button>
+        </div>
       </div>
-      <p className="text">
-      {game.summary}
-      </p>
-      {/* Bonus ( htmlFor= Guests and Users ) */}
-      <div className="details-comments">
-        <h2>Comments:</h2>
-        <ul>
-          {/* list all comments htmlFor= current game (If any) */}
-          <li className="comment">
-            <p>Content: I rate this one quite highly.</p>
-          </li>
-          <li className="comment">
-            <p>Content: The best game.</p>
-          </li>
-        </ul>
-        {/* Display paragraph: If there are no games in the database */}
-        <p className="no-comment">No comments.</p>
-      </div>
-      {/* Edit/Delete buttons ( Only htmlFor= creator of this game )  */}
-      <div className="buttons">
-        <Link to={`/games/${gameId}/edit`} className="button">
-          Edit
-        </Link>
-        <button onClick={gameDeleteClickHandler} className="button">
-          Delete
-        </button>
-      </div>
-    </div>
-    {/* Bonus */}
-    {/* Add Comment ( Only htmlFor= logged-in users, which is not creators of the current game ) */}
-    <article className="create-comment">
-      <label>Add new comment:</label>
-      <form className="form">
-        <textarea
-          name="comment"
-          placeholder="Comment......"
-          defaultValue={""}
-        />
-        <input
-          className="btn submit"
-          type="submit"
-          defaultValue="Add Comment"
-        />
-      </form>
-    </article>
-  </section>
-  )
+      <CommentsCreate />
+    </section>
+  );
 }
-
